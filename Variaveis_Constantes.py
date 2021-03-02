1a40ae5bd474c5036070df9c8b89b6d941efd583
Isabelly Dias <isabellydias29@gmail.com>
2021-03-02 17:51:49 -0300

    aula 1 - variaveis e constantes

---
+++ Variaveis_Constantes.py
@@ -0,0 +1,26 @@
+# Variaveis Inteiras
+
+numeros = -3
+numeros_jogos = 14
+numeros_convidados = 15
+
+# Variaveis de ponto flutuante
+
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+#Strings e chars
+
+letra = 'a'
+palavra1 = 'linguagem de programação'
+
+idade = int(input('Digite sua idade '))
+
+print('Sua idade é:', idade)
+
+pH = float(input('Qual o pH do solo durante a última medição?'))
+print('O pH medido foi ', pH)
+
+nome = str(input('Qual o seu nome?'))
+print('Seu nome é', nome)
