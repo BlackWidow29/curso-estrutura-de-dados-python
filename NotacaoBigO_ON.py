8796b7352ab7c5ae6d01e17dfb5fc83d35995d52
Isabelly Dias <isabellydias29@gmail.com>
2021-03-04 22:01:16 -0300

    Aula 2 sobre Notação Big O

--- NotacaoBigO_ON.py
+++ NotacaoBigO_ON.py
@@ -1,3 +1,4 @@
+#11 passos O(n)
 def soma1(n):
     soma = 0
     for i in range(n + 1):
@@ -5,6 +6,30 @@ def soma1(n):
 
     return soma
 
+
 print(soma1(10))
 
-#print(%timeit soma1(10))
+
+# print(%timeit soma1(10))
+
+#3 passos - O(1)
+def soma2(n):
+    return (n * (n + 1)) / 2
+
+print(soma2(10))
+
+def lista1():
+    lista = []
+    for i in range(1000):
+        lista += 1
+    return lista
+
+print(lista1())
+
+def lista2():
+  return range(1000)
+
+l = lista2()
+
+for i in l:
+  print(i)
